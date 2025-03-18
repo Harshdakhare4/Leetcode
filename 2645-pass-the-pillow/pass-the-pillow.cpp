0bcdb8e77c433d5c2775f7e5ff2e8f4b2da478e4
Harsh Dakhare <121993868+Harshdakhare4@users.noreply.github.com>
2025-03-18 12:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (6.73%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        if (time < n ) { // Pillow moves forward
-            return time + 1;
-        } else {
-            int m = time / (n - 1); // Number of complete forward/backward passes
-            time = time % (n - 1);  // Time left within the current pass
+        int pos = 1, dir = 1; // Start at person 1, moving forward
 
-            if (m % 2 != 0) { // Odd pass → moving backward
-                return n - time;
-            } else { // Even pass → moving forward
-                return time + 1;
+        while (time > 0) {
+            pos += dir;
+            time--;
+
+            if (pos == n || pos == 1) {
+                dir = -dir;
             }
         }
+
+        return pos;
     }
 };
