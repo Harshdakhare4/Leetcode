87526ce25192e3db9094ddb5cefd3dce8cb25fc0
Harsh Dakhare <121993868+Harshdakhare4@users.noreply.github.com>
2025-03-27 11:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (24.01%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int count=0;
+        int i=1;
+       if(n<=3){
+        return false;
+       } 
+       
+       else{
+        while(i<=n){
+         if(n%i==0){
+            count++;
+         }
+         i++;
+        }
+        if(count==3){
+            return true;
+        }
+      
+       }
+       return false;
+    }
+};
